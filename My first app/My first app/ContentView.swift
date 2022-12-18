c0dab8221c95f719dc88ab2f265e462e5d027b58
no00uty <120594501+no00uty@users.noreply.github.com>
2022-12-18 15:01:36 +0300

    New content

--- My first app/My first app/ContentView.swift
+++ My first app/My first app/ContentView.swift
@@ -8,12 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var index = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, hhh!")
+            Text("Natalia Glebova")
+                .foregroundColor(.blue)
+                .font(.largeTitle)
+                .padding(.top, 50)
+            
+            Text("Project manager")
+                .foregroundColor(.black)
+            
+            Spacer()
+            
+            TabView(selection: $index) {
+                            ForEach((1..<21), id: \.self) { index in
+                                Image(String(index))
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                            }
+                        }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always)).frame(height: 250)
+            
+            
+            Button("Start over", action: {index = 0})
+                .padding()
+                .buttonStyle(.bordered)
+                .tint(.blue)
+            Link("LinkedIn Profile", destination: URL(string: "https://www.linkedin.com/in/natalia-glebova-113423113/")!)
+                .foregroundColor(.black)
+            Spacer()
         }
         .padding()
     }
